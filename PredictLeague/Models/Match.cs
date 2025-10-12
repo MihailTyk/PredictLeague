f1e9965018a2c7fa226aa4dcc9f776fcd7bfe0b3
RTX <RTX@DESKTOP-K7GB474>
2025-10-12 18:49:50 +0300

    Add project files.

---
+++ PredictLeague/Models/Match.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PredictLeague.Models
+{
+    public class Match
+    {
+        public int Id { get; set; }
+        public string HomeTeam { get; set; }
+        public string AwayTeam { get; set; }
+        public DateTime StartTime { get; set; }
+        public bool IsFinished { get; set; } = false;
+        public int? HomeScore { get; set; }
+        public int? AwayScore { get; set; }
+    }
+}
