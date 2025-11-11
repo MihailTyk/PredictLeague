9e9f2995efa94f3bd4a2ca4d7eab9fd22a371c19
00 <00>
2025-11-11 14:57:17 +0200

    Добавени „Моите предикти“, обновена навигация и модерен дизайн за Login/Register

---
+++ PredictLeague/Controllers/MyPredictionsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PredictLeague.Data;
+
+namespace PredictLeague.Controllers
+{
+    [Authorize] // трябва да е логнат
+    public class MyPredictionsController : Controller
+    {
+        private readonly PredictLeagueContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public MyPredictionsController(PredictLeagueContext context, UserManager<IdentityUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var userName = User.Identity.Name;
+
+            var predictions = await _context.Prediction
+                .Include(p => p.Match)
+                .Where(p => p.UserName == userName)
+                .OrderByDescending(p => p.CreatedAt)
+                .ToListAsync();
+
+            return View(predictions);
+        }
+    }
+}
