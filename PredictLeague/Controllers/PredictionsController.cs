886c4c1b60c10d94327272b8ac2b8c13ec9a9418
MihailTyk <stoychev0751@abv.bg>
2025-11-16 20:08:41 +0200

    Добавено пренасочване към Login при натискане на Predict за гост потребители

--- PredictLeague/Controllers/PredictionsController.cs
+++ PredictLeague/Controllers/PredictionsController.cs
@@ -6,9 +6,11 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using PredictLeague.Data;
 using PredictLeague.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace PredictLeague.Controllers
 {
+  [Authorize]
     public class PredictionsController : Controller
     {
         private readonly PredictLeagueContext _context;
